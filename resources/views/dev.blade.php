460a2c5665b4dc13e60d3e2302874cb394e98da6
praszxc <bpras747@gmail.com>
2024-09-13 18:16:59 +0700

    15th commit

---
+++ resources/views/dev.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/styleguide.css" />
+    <link rel="stylesheet" href="css/pages/dev/style.css"> 
+    <title>Dev</title>
+</head>
+<body>
+    <div class="hero">
+        <div class="logo">
+          <img src="logo.png" alt="Logo" />
+        </div>
+        <nav class="navbar">
+          <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
+    </div>    
+</body>
+</html>
